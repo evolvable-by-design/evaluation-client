3d0180cb27d629736032d9d69732da08523255c5
Antoine Cheron <cheron.antoine@gmail.com>
2019-11-30 17:52:40 +0100

    Add link to home in the app menu

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Pane, Text, majorScale } from 'evergreen-ui'
+import { Link } from 'react-router-dom'
+import { Pane, Heading, majorScale } from 'evergreen-ui'
 
 import UserProfileBubble from './UserProfileBubble'
 
@@ -7,7 +8,7 @@ function Header({ width }) {
   return (
     <Pane width={ width || majorScale(9) } background='#0747A6' display="flex" flexDirection="column" justifyContent="space-between" paddingY={majorScale(2)} paddingX={majorScale(1)}>
       <Pane display="flex" flexDirection="column" justifyContent="flext-start">
-        <Text color='white'><b>Gira</b></Text>
+        <Link to="/"><Heading size={600} color='white'><b>Gira</b></Heading></Link>
       </Pane>
       <Pane display="flex" flexDirection="column" justifyContent="flext-end">
         <UserProfileBubble />
